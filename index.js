7ef86541ec49bd608a903a311914ab216f2b4872
Jiayu Fan <Klein2339@gmail.com>
2021-01-04 11:50:17 +0000

    initial structure

---
+++ index.js
@@ -0,0 +1,46 @@
+import session from 'express-session';
+import passport from './authenticate';
+import dotenv from 'dotenv';
+import express from 'express';
+import moviesRouter from './api/movies';
+import bodyParser from 'body-parser';
+import './db';
+import {loadUsers, loadMovies} from './seedData';
+import usersRouter from './api/users';
+import genreRouter from './api/genres';
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
+};
+
+//session middleware
+app.use(session({
+  secret: 'ilikecake',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(express.static('public'));
+//configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/genres', genreRouter);
+app.use(errHandler);
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
+});
+if (process.env.SEED_DB) {
+  loadUsers();
+  loadMovies();
+}

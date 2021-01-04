6d76146c01a235e90505acc825066a7a155ba82c
Jiayu Fan <Klein2339@gmail.com>
2021-01-04 16:54:25 +0000

    CICD: Test people endpoints

--- tests/functional/api/genres/index.js
+++ tests/functional/api/genres/index.js
@@ -1,6 +1,7 @@
 import chai from "chai";
 import request from "supertest";
-
+const mongoose = require("mongoose");
+import Genre from "../../../../api/genres/genreModel";
 import dotenv from 'dotenv';
 
 const expect = chai.expect;
@@ -8,37 +9,41 @@ dotenv.config();
 
 let api;
 let token = "eyJhbGciOiJIUzI1NiJ9.dXNlcjE.FmYria8wq0aFDHnzYWhKQrhF5BkJbFNN1PqNyNQ7V4M";
-
+const genres = {
+  id: 28,
+  name: "Action",
+};
 
 describe("Genres endpoint", () => {
-
-  beforeEach(async () => {
-    try {
-      api = require("../../../../index");
-
-    } catch (err) {
-      console.error(`failed to Load user Data: ${err}`);
-    }
-  });
-  afterEach(() => {
-    api.close(); // Release PORT 8080
-    delete require.cache[require.resolve("../../../../index")];
-  });
-
-
-  describe("GET /genres ", () => {
-    it("should return 18 genres and a status 200", () => {
-      request(api)
-        .get("/api/genres")
-        .set("Accept", "application/json")
-        .set("Authorization", token)
-
-        .expect(200)
-        .then(res => {
-          expect(res.body).to.be.a("array");
-          expect(res.body.length).to.equal(18);
-
-        });
+   
+    beforeEach(async () => {
+      try {
+        api = require("../../../../index");
+        await Genre.deleteMany({});
+      await Genre.collection.insertMany(genres);
+      } catch (err) {
+        console.error(`failed to Load genre Data: ${err}`);
+      }
     });
-  });
+    afterEach(() => {
+      api.close(); // Release PORT 8080
+      delete require.cache[require.resolve("../../../../index")];
+    });
+  
+  
+    describe("GET /genres ", () => {
+      it("should return 18 genres and a status 200", () => {
+        request(api)
+          .get("/api/genres")
+          .set("Accept", "application/json")
+          .set("Authorization", token)
+
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.a("array");
+            expect(res.body.length).to.equal(18);
+
+          });
+      });
+    });
 });
